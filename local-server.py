9bbf922778b00ee2157c4ba5733d6e2f8557443b
Peter Boin <Peter.Boin@gmail.com>
2018-11-18 23:27:23 +1100

    about, logo, and minor formatting

--- local-server.py
+++ local-server.py
@@ -9,4 +9,7 @@ Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print "serving at port", PORT
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("[Ctrl+C] server stopped")
